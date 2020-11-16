a6ec9e8178b5c744a61b2719a17c3bdb6803726b
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-11-16 10:00:34 +0000

    Theme updated with woocommerce customisations

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,27 @@
 console.log('Cart Page JS Loaded');
-// Only show this on the cart JS 
+// Only show this on the cart JS
+
+
+//Removal of the individual prodsuct link.
+// const productNameAnchorRemove = document.querySelectorAll('td.product-name a');
+// console.log(productNameAnchorRemove);
+
+// productNameAnchorRemove.forEach(function (link) {
+//     console.log(link.href);
+
+//     productNameAnchorRemove.href = "";
+
+//     console.log(`New link ULR ==  ${link.href}`);
+// });
+
 
 //Checking every second as customer might remove all items in the cart.
-function timed() {
-    console.log('One second elapsed');
+// function timed() {
+//     console.log('One second elapsed');
 
-    //Need to capture link for back to shop button
-    const backBtn = document.querySelector('.wc-backward');
-    //Change the btn url
-    backBtn.href = "/product-category/exhibition-on-screen-2/";
-}
-setInterval(timed, 1000);
+//     //Need to capture link for back to shop button
+//     const backBtn = document.querySelector('.wc-backward');
+//     //Change the btn url
+//     backBtn.href = "/product-category/exhibition-on-screen-2/";
+// }
+// setInterval(timed, 1000);
