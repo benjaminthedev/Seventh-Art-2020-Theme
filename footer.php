6c546b51ac586f1e54c52bdb8fe3bede5c75d0a6
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-11-25 14:01:19 +0000

    DropDown Menu Added

--- footer.php
+++ footer.php
@@ -25,7 +25,8 @@
 		<div class="footer__address">
 			<p>Seventh Art Productions, 63 Ship Street, Brighton, BN1 1AE, UK</p>
 			<p>Telephone: +44 (01273) 777 678</p>
-			<a href="mailto:info@seventh-art.com">info@seventh-art.com</a>
+			<p><a href="mailto:info@seventh-art.com">info@seventh-art.com</a></p>
+			
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
